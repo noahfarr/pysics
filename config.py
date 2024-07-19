1f483afa8f77bea929038670c96e65ca336322d8
Noah Farr <login@noahfarr.com>
2024-07-19 23:27:49 +0200

    Add config.py cli

---
+++ config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    n_particles: int = 500
+    total_timesteps: int = 1000
+    dt: float = 0.01
+    gravity: float = -9.81
+    size: tuple[int, int] = (1200, 800)
+    target_fps: int = 60
+    render: bool = False
+    wall_damp: float = 0.95
+    restitution: float = 0.95
---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tyro
+
+from config import Config
+from pysics.simulation import Simulation
+from pysics.render import Renderer, RaylibRenderer
+
+
+def main(config: Config):
+
+    renderer: Renderer = RaylibRenderer(size=config.size, target_fps=config.target_fps)
+
+    simulation = Simulation(
+        n_particles=config.n_particles,
+        gravity=config.gravity,
+        dt=config.dt,
+        renderer=renderer,
+        WALL_DAMP=config.wall_damp,
+        RESTITUTION=config.restitution,
+    )
+    simulation.simulate(total_timesteps=config.total_timesteps, render=config.render)
+
+
+if __name__ == "__main__":
+    config = tyro.cli(Config)
+    main(config)
